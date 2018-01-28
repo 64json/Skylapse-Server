7c3c951047e3dd2c85dd9101d4d92950d750a5e6
Jason Park <parkjs814@gmail.com>
2018-01-27 22:51:07 -0600

    Fix webhook again

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const bodyParser = require('body-parser');
 const randomstring = require('randomstring');
 const morgan = require('morgan');
 const request = require('request');
+const crypto = require('crypto');
+const concat = require('concat-stream');
 const { spawn } = require('child_process');
 const { mkdir, rmdir, readable } = require('./utils');
 const { millisecond, second, minute, hour, day, week, month, year } = require('./units');
@@ -14,6 +16,7 @@ const {
   PASSWORD,
   PAGE_ID,
   ACCESS_TOKEN,
+  WEBHOOK_SECRET,
 } = process.env;
 
 const output = path.resolve(__dirname, 'output');
@@ -38,6 +41,21 @@ const upload = multer({ storage });
 let randomEndpoint = null;
 
 app.use(morgan('dev'));
+
+app.post('/apply_release', (req, res) => {
+  req.pipe(concat(data => {
+    const hmac = crypto.createHmac('sha1', WEBHOOK_SECRET);
+    const signature_delivered = req.headers['x-hub-signature'];
+    const signature_created = 'sha1=' + hmac.update(data).digest('hex');
+    if (signature_delivered !== signature_created) return res.status(500).send({});
+    res.send({});
+    const command = spawn('sh', [path.resolve(__dirname, 'bin', 'pull.sh')]);
+    command.stdout.on('data', data => console.info(data.toString()));
+    command.stderr.on('data', data => console.error(data.toString()));
+    command.on('exit', code => console.info(`child process exited with code ${code}`));
+  }));
+});
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
